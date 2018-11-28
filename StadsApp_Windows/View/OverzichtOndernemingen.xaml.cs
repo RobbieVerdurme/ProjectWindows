d85c04b3976907e39a9892515a8edad393af12fc
Robbie verdurme <boeferrob@live.be>
2018-11-28 10:14:49 +0100

    map in overzichtondernemingen

--- StadsApp_Windows/View/OverzichtOndernemingen.xaml.cs
+++ StadsApp_Windows/View/OverzichtOndernemingen.xaml.cs
@@ -7,10 +7,12 @@ using System.Diagnostics;
 using System.IO;
 using System.Linq;
 using System.Runtime.InteropServices.WindowsRuntime;
+using Windows.Devices.Geolocation;
 using Windows.Foundation;
 using Windows.Foundation.Collections;
 using Windows.UI.Xaml;
 using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Maps;
 using Windows.UI.Xaml.Controls.Primitives;
 using Windows.UI.Xaml.Data;
 using Windows.UI.Xaml.Input;
@@ -38,6 +40,7 @@ namespace StadsApp_Windows.View
         {
             base.OnNavigatedTo(e);
             overzichtvm = new OverzichtOndernemingenViewModel();
+            ShowMapAsync();
 			await overzichtvm.GetData();
             this.DataContext = overzichtvm;
         }
@@ -54,7 +57,37 @@ namespace StadsApp_Windows.View
             overzichtvm.ZoekOnderneming(txtZoekOnderneming.Text);
         }
 
+        public async void ShowMapAsync()
+        {
+            var accessStatus = await Geolocator.RequestAccessAsync();
+            switch (accessStatus)
+            {
+                case GeolocationAccessStatus.Allowed:
+                    Geolocator geolocator = new Geolocator { DesiredAccuracyInMeters = 100, ReportInterval=10000 };
+                    Geoposition pos = await geolocator.GetGeopositionAsync().AsTask();
 
+                    BasicGeoposition begin = new BasicGeoposition();
+                    begin.Latitude = pos.Coordinate.Latitude;
+                    begin.Longitude = pos.Coordinate.Longitude;
+
+                    Geopoint location = new Geopoint(begin);
+
+                    MapIcon posIcon1 = new MapIcon();
+                    posIcon1.Location = location;
+                    posIcon1.NormalizedAnchorPoint = new Point(0.5, 1.0);
+                    posIcon1.Title = "Uw Locatie";
+                    posIcon1.ZIndex = 0;
+                    MyMap.MapElements.Add(posIcon1);
+
+                    MyMap.Center = location;
+                    MyMap.ZoomLevel = 14;
+                    break;
+
+                case GeolocationAccessStatus.Denied:
+                    break;
+            }
+
+        }
 
 
 
