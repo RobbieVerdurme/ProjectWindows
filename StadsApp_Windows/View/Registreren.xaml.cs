e30b2c24bae82ca662a18c6b2715a9a16361cdd3
angelocarlyhogent <angelo.carly.y7553@student.hogent.be>
2018-12-02 18:09:34 +0100

    update login screen

--- StadsApp_Windows/View/Registreren.xaml.cs
+++ StadsApp_Windows/View/Registreren.xaml.cs
@@ -24,6 +24,7 @@ namespace StadsApp_Windows.View
     public sealed partial class Registreren : Page
     {
         private RegistrerenViewModel registrerenvm;
+
         public Registreren()
         {
             this.InitializeComponent();
@@ -31,14 +32,29 @@ namespace StadsApp_Windows.View
             this.DataContext = registrerenvm;
         }
 
-        private void RegistreerClicked(object sender, RoutedEventArgs e)
-        {
-            registrerenvm.Registreer(txtUsername, txtPasword, txtPasswordConfermation);
-        }
-
         private void BackClicked(object sender, RoutedEventArgs e)
         {
             Frame.Navigate(typeof(Login));
         }
-    }
+
+		private async void RegisterButton_Click_Async(object sender, RoutedEventArgs e)
+		{
+			ErrorMessage.Text = "";
+
+			//In the real world you would normally validate the entered credentials and information before 
+			//allowing a user to register a new account. 
+			//For this sample though we will skip that step and just register an account if username is not null.
+
+			if (!string.IsNullOrEmpty(UsernameTextBox.Text))
+			{
+				
+				//Navigate to the Welcome Screen. 
+				//Frame.Navigate(typeof(Welcome), _account);
+			}
+			else
+			{
+				ErrorMessage.Text = "Please enter a username";
+			}
+		}
+	}
 }
