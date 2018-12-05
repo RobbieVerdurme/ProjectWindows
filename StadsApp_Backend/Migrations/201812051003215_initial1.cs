a4c25cb74c3b96f1a1884a8b5663a403fc0ef3ed
Robbie verdurme <boeferrob@live.be>
2018-12-05 11:36:30 +0100

    d

---
+++ StadsApp_Backend/Migrations/201812051003215_initial1.cs
@@ -0,0 +1,16 @@
+namespace StadsApp_Backend.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class initial1 : DbMigration
+    {
+        public override void Up()
+        {
+        }
+        
+        public override void Down()
+        {
+        }
+    }
+}
