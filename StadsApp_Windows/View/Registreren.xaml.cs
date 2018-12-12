960d10775a1fe16ae033cd7437ae3ae3309dddb2
angelocarlyhogent <angelo.carly.y7553@student.hogent.be>
2018-12-12 16:12:56 +0100

    implement registration

--- StadsApp_Windows/View/Registreren.xaml.cs
+++ StadsApp_Windows/View/Registreren.xaml.cs
@@ -1,8 +1,11 @@
-﻿using StadsApp_Windows.ViewModel;
+﻿using Newtonsoft.Json;
+using StadsApp_Backend.Models;
+using StadsApp_Windows.ViewModel;
 using System;
 using System.Collections.Generic;
 using System.IO;
 using System.Linq;
+using System.Net.Http;
 using System.Runtime.InteropServices.WindowsRuntime;
 using Windows.Foundation;
 using Windows.Foundation.Collections;
@@ -40,21 +43,50 @@ namespace StadsApp_Windows.View
 		private async void RegisterButton_Click_Async(object sender, RoutedEventArgs e)
 		{
 			ErrorMessage.Text = "";
+			
 
 			//In the real world you would normally validate the entered credentials and information before 
 			//allowing a user to register a new account. 
 			//For this sample though we will skip that step and just register an account if username is not null.
 
-			if (!string.IsNullOrEmpty(UsernameTextBox.Text))
-			{
-				
-				//Navigate to the Welcome Screen. 
-				//Frame.Navigate(typeof(Welcome), _account);
-			}
-			else
+			if (string.IsNullOrEmpty(UsernameTextBox.Text))
 			{
 				ErrorMessage.Text = "Please enter a username";
+				return;
 			}
+			if (string.IsNullOrEmpty(PasswordBox.Password))
+			{
+				ErrorMessage.Text = "Please enter a password";
+				return;
+			}
+			if (string.IsNullOrEmpty(PasswordRepeatBox.Password))
+			{
+				ErrorMessage.Text = "Please enter the password validation";
+				return;
+			}
+			if (PasswordBox.Password != PasswordRepeatBox.Password)
+			{
+				ErrorMessage.Text = "Passwords do not match";
+				return;
+			}
+			
+			//Register user
+			var registerdata = new RegisterBindingModel() { Email = UsernameTextBox.Text, Password = PasswordBox.Password, ConfirmPassword = PasswordRepeatBox.Password };
+			var registerJson = JsonConvert.SerializeObject(registerdata);
+			HttpClient client = new HttpClient();
+			var res = await client.PostAsync("http://localhost:53331/api/account/register", new
+			StringContent(registerJson, System.Text.Encoding.UTF8, "application/json"));
+
+			//Log error message
+			if(!res.IsSuccessStatusCode)
+			{
+				ErrorMessage.Text = res.ReasonPhrase;
+				return;
+			}
+
+			//Go to main page
+			Frame.Navigate(typeof(MainPage));
+
 		}
 	}
 }
