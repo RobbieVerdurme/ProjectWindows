697f294e7552ec867b52887381e61080be4a3647
Robbie verdurme <boeferrob@live.be>
2019-07-03 22:27:18 +0200

    adress to location bing api

--- StadsApp_Windows/View/VestigingAanmaken.xaml.cs
+++ StadsApp_Windows/View/VestigingAanmaken.xaml.cs
@@ -9,8 +9,10 @@ using System.IO;
 using System.Linq;
 using System.Runtime.InteropServices.WindowsRuntime;
 using System.Threading.Tasks;
+using Windows.Devices.Geolocation;
 using Windows.Foundation;
 using Windows.Foundation.Collections;
+using Windows.Services.Maps;
 using Windows.UI.Xaml;
 using Windows.UI.Xaml.Controls;
 using Windows.UI.Xaml.Controls.Primitives;
@@ -52,7 +54,12 @@ namespace StadsApp_Windows.View
 
         private async void VestigingOpslaan(object sender, RoutedEventArgs e)
         {
-            Vestiging vestiging = new Vestiging(GeselecteerdeOnderneming.OndernemingID, txtNaam.Text, txtAdres.Text);
+            Geopoint gp = new Geopoint(new BasicGeoposition());
+            MapLocationFinderResult res = await MapLocationFinder.FindLocationsAsync(txtAdres.Text, gp);
+            MapLocation location = res.Locations.First();
+
+            Vestiging vestiging = new Vestiging(GeselecteerdeOnderneming.OndernemingID, txtNaam.Text, txtAdres.Text, location.Point.Position.Latitude, location.Point.Position.Longitude);
+
             await vestigingvm.AanmakenVestigingAsync(vestiging);
             GeselecteerdeOnderneming.Vestigingen.Add(vestiging);
             this.Frame.Navigate(typeof(OndernemingDetail), new ParamDTO()
