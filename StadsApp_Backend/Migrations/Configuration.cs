dcd7aabc1314273586f9120e8575b731eba30bb7
angelocarly <angelo.carly@hotmail.com>
2019-08-26 01:08:37 +0200

    Bugfix update database overwrite

--- StadsApp_Backend/Migrations/Configuration.cs
+++ StadsApp_Backend/Migrations/Configuration.cs
@@ -4,13 +4,17 @@ namespace StadsApp_Backend.Migrations
     using System;
     using System.Data.Entity;
     using System.Data.Entity.Migrations;
+    using System.Data.Entity.Validation;
+    using System.Diagnostics;
     using System.Linq;
+    using System.Text;
 
     internal sealed class Configuration : DbMigrationsConfiguration<StadsApp_Backend.Models.ApplicationDbContext>
     {
         public Configuration()
         {
-            AutomaticMigrationsEnabled = false;
+            AutomaticMigrationsEnabled = true;
+            AutomaticMigrationDataLossAllowed = true;
         }
 
         protected override void Seed(StadsApp_Backend.Models.ApplicationDbContext context)
@@ -19,8 +23,36 @@ namespace StadsApp_Backend.Migrations
 
             //  You can use the DbSet<T>.AddOrUpdate() helper extension method 
             //  to avoid creating duplicate seed data.
-            context.Roles.Add(new IdentityRole("Ondernemer"));
-            context.Roles.Add(new IdentityRole("Gebruiker"));
+            context.Roles.AddOrUpdate(x => x.Name, new IdentityRole("Ondernemer"));
+            context.Roles.AddOrUpdate(x => x.Name, new IdentityRole("Gebruiker"));
+            SaveChanges(context);
+        }
+
+        private void SaveChanges(DbContext context)
+        {
+            try
+            {
+                context.SaveChanges();
+            }
+            catch (DbEntityValidationException ex)
+            {
+                StringBuilder sb = new StringBuilder();
+
+                foreach (var failure in ex.EntityValidationErrors)
+                {
+                    sb.AppendFormat("{0} failed validation\n", failure.Entry.Entity.GetType());
+                    foreach (var error in failure.ValidationErrors)
+                    {
+                        sb.AppendFormat("- {0} : {1}", error.PropertyName, error.ErrorMessage);
+                        sb.AppendLine();
+                    }
+                }
+
+                throw new DbEntityValidationException(
+                    "Entity Validation Failed - errors follow:\n" +
+                    sb.ToString(), ex
+                ); // Add the original exception as the innerException
+            }
         }
     }
 }
